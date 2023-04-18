62f7c3534f2f207581231f75e28719bb105395e0
Marco Mazza <marcomazza@Air-di-Marco.home-life.hub>
2023-04-18 14:58:06 +0200

    inzio progetto

---
+++ main.js
@@ -0,0 +1,10 @@
+// Rappresentare un MUSIC Player che contiene una lista di brani musicali:
+// Implementare le seguenti funzionalita':
+
+// - Animazione disco che gira
+// - Funzione Play
+// - Funzione Pausa
+// - Funzione Next
+// - Funzione Prev
+// - Tempo che scorre
+// - Durata brano
